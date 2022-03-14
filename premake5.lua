0674545b2bdc1d4d8f77a700acd53fd14237c61b
MicKr4ne <47811288+MicKr4ne@users.noreply.github.com>
2022-03-14 08:50:17 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -4,7 +4,7 @@ project "GLFW"
 	staticruntime "on"
     
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
@@ -16,17 +16,21 @@ project "GLFW"
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
     	}
     
 	filter "system:windows"
-		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
 		staticruntime "Off"
 
 		files
 		{
 			"src/win32_init.c",
+			"src/win32_module.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
@@ -42,7 +46,10 @@ project "GLFW"
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
-    	filter { "system:windows", "configurations:Release" }
-    		buildoptions "/MD"
-	filter { "system:windows", "configurations:Debug" }
-    		buildoptions "/MDd"
+    	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
